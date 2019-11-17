91bd9610c187d5a8fc86bedcf07e84a4d59ccb9e
Anthony Mai <anthony.mai530@gmail.com>
2019-11-17 01:25:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ monopoly = {:railroads => {:pieces => 4}}
 end
 
 def monopoly_with_third_tier
-monopoly = {:railroads => {:pieces => 4}, {:names => {}} }
+monopoly = {:railroads => {:pieces => 4} }
 end
 
 def monopoly_with_fourth_tier
